6b34c8c909fdbd0d446bf9041196a6d983a280e6
David Gonzalez <davidgonzalez@Davids-MacBook-Air.local>
2023-03-24 20:25:25 -0700

    Add Get customers for Manager controller

--- BankBackEnd/Controllers/ManagerController.cs
+++ BankBackEnd/Controllers/ManagerController.cs
@@ -1,15 +1,44 @@
+using BankBackEnd.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankBackEnd.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize(Roles = "Manager,Admin")]
+    // ** Bottom code will tell authentication to authorize a user
+    // ** if they are in both roles. EX: Manager AND Admin roles in
+    // ** order to access this controller
+    // [Authorize(Roles = "Admin")]
     public class ManagerController : ControllerBase
     {
-        [HttpGet]
-        public void Get()
+        
+        private readonly DBContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public ManagerController(
+            UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager,
+            DBContext context
+        )
         {
-            System.Console.WriteLine("Get");
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        [HttpGet("get-all-customers")]
+        public IActionResult GetAllCustomers()
+        {
+            // Todo: Have this filter just customers as managers should not see admin accounts
+            // var getCustomers = _context.Users.ToList();
+            var getCustomers = _userManager.Users.ToList();
+            var adminUsers = _roleManager.Roles.Single(r => r.Name == "Admin");
+
+            // var userList = new List<IdentityUserRole>();
+            return Ok();
         }
 
         [HttpPost]
